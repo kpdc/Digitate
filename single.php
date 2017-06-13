7020e3b0ffb7c05b33f7271687a3c409406ad9b0
danc <danc@nowcomms.asia>
2017-06-13 19:45:01 +0800

    Update Blog Title to Capital B

--- single.php
+++ single.php
@@ -13,8 +13,10 @@ get_header(); ?>
         <main id="main" class="site-main" role="main">
 
             <header class="single-header">
-                <h2 class="page-title"><img src="<?php bloginfo('template_directory') ?>/images/digitate.png">blog</h2>
+                <?php if(is_singular('post')) : ?>
+                <h2 class="page-title"><img src="<?php bloginfo('template_directory') ?>/images/digitate.png">Blog</h2>
                 <?php get_search_form(); ?>
+                <?php endif; ?>
             </header>
 
             <?php
