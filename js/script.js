3b96c4be30f54c67af2c87433eca6f21fdc52323
danc <danc@nowcomms.asia>
2017-06-15 18:09:49 +0800

    Update script for smooth scrolling effect

--- js/script.js
+++ js/script.js
@@ -91,4 +91,18 @@ jQuery(document).ready(function($) {
 
 	// remove p tag that wrap the img
 	$('p > img').unwrap();
+
+	// ScrollTo
+	$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 100
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
