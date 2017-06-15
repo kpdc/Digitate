84f2cda0b15a83d4a76777e4369b453054a5e832
danc <danc@nowcomms.asia>
2017-06-15 08:56:54 +0800

    Fix About and Partner Page Alignment

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -54,7 +54,7 @@
             </div>
         <?php } ?>
         
-        <?php if(!is_page( array('learn', 'resolve', 'prevent', 'banking-financial-services', 'consumer-goods', 'insurance', 'power-and-energy', 'retail', 'telecom', 'ignio-for-it-operations', 'ignio-for-batch', 'ignio-for-sap') )) { ?>
+        <?php if(!is_page( array('learn', 'resolve', 'prevent', 'banking-financial-services', 'consumer-goods', 'insurance', 'power-and-energy', 'retail', 'telecom', 'ignio-for-it-operations', 'ignio-for-batch', 'ignio-for-sap', 'about','partner') )) { ?>
             <header class="entry-header">
                     <?php the_title( '<h2 class="page-title">', '</h2>' );
         
