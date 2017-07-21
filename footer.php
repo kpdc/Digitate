d1b3dc991ce3a7ad9817c593eade42cbbb0c689f
KP <kevin.park@digitate.com>
2017-07-21 02:15:16 +0000

    Now Comms footer changes

--- footer.php
+++ footer.php
@@ -11,74 +11,69 @@
 
 ?>
 
-            </div><!-- .site-content -->
-        </div><!-- .container -->
-        
-        <footer class="site-footer">
-            <div class="wrap">
-                <img src="<?php bloginfo('template_directory'); ?>/images/digitate.png" alt="digitate" class="footBrand">
-                <div class="contact-group">
-                    <div class="branches-info">
-                        <div class="address">
-                            5201 Great America Parkway<br>
-                            Suite 522<br>
-                            Santa Clara, CA 95054<br>
-                            USA
-                        </div> <!-- .address -->
-                        <div class="address">
-                            Sahyadri Park, Plot No.2 &amp; 3<br>
-                            Rajiv Gandhi Infotech Park<br>
-                            Phase-III<br>
-                            Hinjewadi, Pune 411057<br>
-                            Maharashtra<br>
-                            India
-                        </div> <!-- .address -->
-                    </div> <!-- .branches-info -->
-                    <nav class="foot-nav">
+        </div><!-- .site-content -->
+    </div><!-- .container -->
+
+    <footer class="site-footer">
+        <div class="wrap">
+            <img src="<?php bloginfo('template_directory'); ?>/images/digitate.png" alt="digitate" class="footBrand">
+            <div class="contact-group">
+                <div class="foot-info">
+                    <div class="address">
+                        5201 Great America Parkway<br>
+                        Suite 522<br>
+                        Santa Clara, CA 95054<br>
+                        USA
+                    </div> <!-- .address -->
+                </div> <!-- .foot-info -->
+                <div class="foot-info">
+                    <div class="address">
+                        Sahyadri Park, Plot No.2 &amp; 3<br>
+                        Rajiv Gandhi Infotech Park<br>
+                        Phase-III<br>
+                        Hinjewadi, Pune 411057<br>
+                        Maharashtra<br>
+                        India
+                    </div> <!-- .address -->
+                </div> <!-- .foot-info -->
+                <div class="foot-info">
+                    <nav class="legal">
                         <?php
                             wp_nav_menu(array(
-                                'theme_location' => 'footsitemap',
+                                'theme_location' => 'legal',
                                 'container' => '',
                             ));
                         ?>
-                    </nav> <!-- .foot-nav -->
-                    <div class="others">
-                        <ul class="awards">
-                            <li><a href="http://digitate.com/digitate-wins-best-enterprise-application-for-artificial-intelligence-at-ai-summit/"><img src="<?php bloginfo('template_directory'); ?>/images/awards_1.jpg" height="73" width="57" alt=""></a></li>
-                            <li><a href="http://digitate.com/digitate-earns-big-innovation-award-recognition/"><img src="<?php bloginfo('template_directory'); ?>/images/awards_2.png" height="72" width="80" alt=""></a></li>
-                        </ul> <!-- .others -->
-                        <nav class="legal">
-                            <?php
-                                wp_nav_menu(array(
-                                    'theme_location' => 'legal',
-                                    'container' => '',
-                                ));
-                            ?>
-                        </nav> <!-- .legal -->
-                        <nav class="social">
-                            <?php
-                                wp_nav_menu(array(
-                                    'theme_location' => 'social',
-                                    'container' => '',
-                                ));
-                            ?>
-                        </nav> <!-- .social -->
-                    </div> <!-- .others -->
-                </div> <!-- .contact-group -->
-            </div> <!-- .wrap -->
-        </footer><!-- .site-footer -->
-        <div class="demo">
-                <p class="demo-button">Request a Demo</p>
-                <div class="eform">
-                    <div class="close">x</div>
-                    <?php echo do_shortcode( '[contact-form-7 id="47" title="Request a Demo"]' ); ?>
-                </div>
+                    </nav> <!-- .legal -->
+                    <nav class="social">
+                        <?php
+                            wp_nav_menu(array(
+                                'theme_location' => 'social',
+                                'container' => '',
+                            ));
+                        ?>
+                    </nav> <!-- .legal -->
+                </div> <!-- .foot-info -->
+                <div class="foot-info">
+                    <ul class="awards">
+                        <li><a href="/digitate-wins-best-enterprise-application-for-artificial-intelligence-at-ai-summit/"><img src="<?php bloginfo('template_directory'); ?>/images/awards_1.jpg" height="73" width="57" alt=""></a></li>
+                        <li><a href="/digitate-earns-big-innovation-award-recognition/"><img src="<?php bloginfo('template_directory'); ?>/images/awards_2.png" height="72" width="80" alt=""></a></li>
+                    </ul> <!-- .awards -->
+                </div> <!-- .foot-info -->
+            </div> <!-- .contact-group -->
+        </div> <!-- .wrap -->
+    </footer>
+
+    <div class="demo">
+        <p class="demo-button">Request a Demo</p>
+        <div class="eform">
+            <div class="close">x</div>
+            <?php echo do_shortcode( '[contact-form-7 id="47" title="Request a Demo"]' ); ?>
         </div>
+    </div>
 </div><!-- .pageWrap -->
 
 <?php wp_footer(); ?>
 
 </body>
 </html>
-
-<?php ob_flush(); ?>
