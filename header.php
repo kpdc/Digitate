68a77c2c7d8c700e0ce09b691468be5873239e14
KP <kevin.park@digitate.com>
2017-09-05 12:25:02 +0000

    1. Added Google analytics

--- header.php
+++ header.php
@@ -28,6 +28,14 @@
 <link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo('template_directory') ?>/icon-144.png">
 
 <?php wp_head(); ?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-71149385-1', 'auto');
+  ga('send', 'pageview');
+</script>
 </head>
 
 <body id="<?php echo (is_page()) ? get_query_var('name') : ((is_home()) ? "home" : ((is_single()) ? "single": ((is_category()) ? single_cat_title() : ((is_archive()) ? "archive" : "")))); ?>" <?php body_class(); ?>>
