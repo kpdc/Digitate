56bc0e05292722287fe049122160b9e60be23a24
danc <danc@nowcomms.asia>
2017-10-26 23:44:32 +0800

    Remove the unneccessary lines at the bottom of the page

--- single.php
+++ single.php
@@ -24,7 +24,9 @@ get_header(); ?>
 
                 get_template_part( 'template-parts/content', 'single' );
 
-                digitate_post_nav();
+                if(is_single('resource')) {
+                    digitate_post_nav();
+                }
 
                 // If comments are open or we have at least one comment, load up the comment template.
                 if ( comments_open() || get_comments_number() ) :
